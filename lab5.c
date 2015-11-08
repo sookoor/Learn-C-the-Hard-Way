70f49ae1c613ba8e1555e5605a66e85de3fa39e7
Tamim Sookoor <tamimsookoor@gmail.com>
2015-11-08 18:08:40 -0500

    Replaces unsafe gets with fgets

--- lab5.c
+++ lab5.c
@@ -9,19 +9,28 @@ struct Employee {
     int salary;
 };
 
+void stripNewline(char *str) {
+    int len = strlen(str);
+    if (len > 0 && str[len-1] == '\n')
+        str[len-1] = '\0';
+}
+
 int main() {
     struct Employee employees[BUFSIZ];
     for (int i = 0; i < BUFSIZ; i++) {
         printf("Enter the last name: ");
         fflush(stdout); /* To keep cursor on same line as prompt */
-        gets(employees[i].last);
+        fgets(employees[i].last, sizeof employees[i].last, stdin);
+        stripNewline(employees[i].last);
         if (strlen(employees[i].last) > 0) {
             printf("Enter the first name: ");
             fflush(stdout);
-            gets(employees[i].first);
+            fgets(employees[i].first, sizeof employees[i].first, stdin);
+            stripNewline(employees[i].first);
             printf("Enter the job title: ");
             fflush(stdout);
-            gets(employees[i].title);
+            fgets(employees[i].title, sizeof employees[i].title, stdin);
+            stripNewline(employees[i].title);
             printf("Enter the salary: ");
             fflush(stdout);
             scanf("%d", &employees[i].salary);
